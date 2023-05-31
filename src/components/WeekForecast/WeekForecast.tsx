3de21eae416d2b7662343760a938e9176de98863
Bogdan <bodia-00wot@ukr.net>
2023-05-31 23:30:54 +0300

    added weekForecastList

--- src/components/WeekForecast/WeekForecast.tsx
+++ src/components/WeekForecast/WeekForecast.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native';
 import { styles } from './styles';
 import Calendar from '../../../assets/images/calendar.svg';
+import { WeekForecastList } from '../WeekForecastList';
 
 export const WeekForecast = () => {
   return ( 
@@ -9,6 +10,7 @@ export const WeekForecast = () => {
         <Text style={styles.title}>Next Forecast</Text>
         <Calendar width={30} height={30}></Calendar>
       </View>
+      <WeekForecastList />
     </View>
   );
 };
---
+++ src/components/WeekForecastList/WeekForecastList.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+
+export const WeekForecastList = () => {
+  return ( 
+    <View style={styles.container}>
+    </View>
+  );
+};
