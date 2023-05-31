d4a9a31407400cc36102d78fa22df294deeade22
Bogdan <bodia-00wot@ukr.net>
2023-05-31 22:11:55 +0300

    added list

---
+++ src/components/ListOfHourCards/ListOfHourCards.tsx
@@ -0,0 +1,19 @@
+import { View, Text, ScrollView } from 'react-native';
+import { styles } from './styles';
+import { InfoCard } from '../InfoCard';
+import { InfoHourCard } from '../infoHourCard';
+
+export const ListOfHourCards = () => {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <InfoHourCard isActive={false}/>
+        <InfoHourCard isActive={true}/>
+        <InfoHourCard isActive={false}/>
+        <InfoHourCard isActive={false}/>
+        <InfoHourCard isActive={false}/>
+        <InfoHourCard isActive={false}/>
+      </View>
+    </ScrollView>
+  );
+};
