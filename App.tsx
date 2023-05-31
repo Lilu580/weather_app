a64104316736c9708f41900f5951b8ca72014275
Bogdan <bodia-00wot@ukr.net>
2023-05-31 18:41:52 +0300

    started create header

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './src/components/Header';
 
 export default function App() {
   return (
-    <LinearGradient colors={['#fff', '#000']} style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </LinearGradient>
+      <LinearGradient colors={['#08244F', '#134CB5', '#0B42AB']} style={styles.container}>
+        <Header />
+      </LinearGradient>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: '100%',
+    paddingTop: 70,
+    paddingHorizontal: 30.
   },
 });
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import Location from '../../../assets/images/Location.svg';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Location width={24} height={20}/>
+      <Text> Kyiv </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: 30,
+  },
+  city: {
+
+  }
+});
